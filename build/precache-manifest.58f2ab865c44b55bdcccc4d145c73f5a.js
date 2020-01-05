89e6ad16ce98283058de118b5ad3251144d865e0
tisanthan <tisanthan12@gmail.com>
2020-01-05 14:07:15 +0530

    initial

---
+++ build/precache-manifest.58f2ab865c44b55bdcccc4d145c73f5a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dae2b20056a9ed5999c4",
+    "url": "/me/static/css/main.71b3de71.chunk.css"
+  },
+  {
+    "revision": "dae2b20056a9ed5999c4",
+    "url": "/me/static/js/main.dae2b200.chunk.js"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "cf1996edf024f44a36764475a2dcbb12",
+    "url": "/me/index.html"
+  }
+];
